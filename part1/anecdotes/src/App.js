5c9b0dcac81f3c3bed0652042484fb8ad76e1ced
justus237 <62303964+justus237@users.noreply.github.com>
2022-11-08 13:34:03 +0000

    add 1.12.-1.14. anecdotes with arrays

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Heading = ( {text} ) => <h1>{text}</h1>
+
+const Anecdote = ({anecdote, points}) => (
+  <div>
+  {anecdote}
+  <br></br>
+  has {points} votes
+  </div>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0);
+  
+  const handleNextRandomAnecdoteClick = () => {
+    const randomSelection = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomSelection)
+  }
+  
+  //implementation with objects, using anecdotes themselves as keys (array would probably be more memory efficient?)
+  // const [points, setPoints] = useState(Object.fromEntries(anecdotes.map(anecdote => [anecdote, 0])))
+  //cannot make it more compact due to babel issues i think
+  // const handleVoteClick = () => {
+  //   const copy = {...points}
+  //   copy[anecdotes[selected]] += 1
+  //   setPoints(copy)
+  // }
+  // getting the anecdote with highest points probably requires a loop over all keys
+  //implementation with arrays
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  
+  const handleVoteClick = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+  
+  const highestPoints = points.indexOf(Math.max(...points))
+
+  //<p>has {points[anecdotes[selected]]} votes</p>
+  return (
+    <div>
+    <Heading text='Anecdote of the day' />
+    <Anecdote anecdote={anecdotes[selected]} points={points[selected]} />
+    <Button handleClick={handleVoteClick} text='vote' />
+    <Button handleClick={handleNextRandomAnecdoteClick} text='next anecdote' />
+    <Heading text='Anecdote with most votes' />
+    <Anecdote anecdote={anecdotes[highestPoints]} points={points[highestPoints]} />
+    </div>
+  );
+}
+
+export default App
